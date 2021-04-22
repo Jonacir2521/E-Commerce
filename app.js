e772ee90264a64b8145a86ae90c117d36d748133
Jonacir2521 <jonacir2521@gmail.com>
2021-04-21 20:28:24 -0400

    Dashboard

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', viewIndex)
+app.get('/produtos', paginaProdutos)
+
+app.listen(3001);
+
+const produtos = [
+    {codigo: 1, desc: 'Retrovisor', preco: 65},
+    {codigo: 2, desc: 'Espelho', preco: 30},
+    {codigo: 3, desc: 'Insulfilme', preco: 180},
+    {codigo: 4, desc: 'Alarme', preco: 450},
+    {codigo: 5, desc: 'Troca de óleo', preco: 60},
+]
+
+function paginaInicial(req,res){
+    res.sendFile(__dirname+'/public/index.html');
+}
+
+function viewIndex(req,res){
+    res.render('index');
+}
+
+function paginaProdutos(req,res){
+    res.render('lista-produtos', {produtos})
+}
